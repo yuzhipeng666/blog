00cb246950df9c662e0287bed35777407d34b22a
yuzhipeng <zhipeng.yu@evenhidata.com>
2018-04-17 15:09:16 +0800

    add

--- resources/views/web/home/homeid.blade.php
+++ resources/views/web/home/homeid.blade.php
@@ -9,7 +9,7 @@
             <!-- Product list begin -->
             <div class="panel-wrap">
                 <div class="panel">
-                    <div class="panel-hd" style="margin-right: 10px!important;margin-left: 10px!important;">
+                    <div class="" style="margin-right: 10px!important;margin-left: 10px!important;">
                         <div id="htmls">{{$newsid->content}}</div>
                     </div>
                 </div>

ff8e0f1147f1feaff0b97da4feed00a1810e2caf
yuzhipeng <zhipeng.yu@evenhidata.com>
2018-04-25 18:09:42 +0800

    op添加明方II LED T5一体化支架连接

--- resources/views/web/home/homeid.blade.php
+++ resources/views/web/home/homeid.blade.php
@@ -17,27 +17,28 @@
 
             </div>
 
-            <div class="panel-wrap">
-                <div class="panel home-contact">
-                    <div class="panel-bd" >
-                        <div class="">
-                            <h3>快象文化传播（上海）有限公司 </h3>
-                            <p><span>地址：</span><a href="">{{$sstt->homedata()->address}}</a></p>
-                            <p><span>电话：</span><a href="tel:{{$sstt->homedata()->phone}}">{{$sstt->homedata()->phone}} </a>
-                            </p>
-                            <p><span>网址：</span><a href="{{$sstt->homedata()->url}}">{{$sstt->homedata()->url}}</a>
-                            </p>
-                            <p><span>备案号：</span><a >沪ICP备17052966号-1</a>
-                            </p>
+        <div class="panel-wrap">
+            <div class="panel home-contact">
 
-                        </div>
-                        <div class="">
-                            <img width="80px" class="lazy"src="{{asset("./web/static/images/WechatIMG5.jpeg")}}">
-                            <img width="80px" class="lazy"src="{{asset("./web/static/images/WechatIMG6.jpeg")}}">
-                        </div>
+                <div class="panel-bd">
+                    <div class="">
+                        <h3>快象文化传播（上海）有限公司 </h3>
+                        <p><span>地址：</span><a href="">{{$sstt->homedata()->address}}</a></p><br/>
+                        <p><span>电话：</span><a href="tel:{{$sstt->homedata()->phone}}">{{$sstt->homedata()->phone}} </a>
+                        </p><br/>
+                        <p><span>网址：</span><a href="{{$sstt->homedata()->url}}">{{$sstt->homedata()->url}}</a>
+                        </p><br/>
+                        <p><span>备案号：</span><a >沪ICP备17052966号-1</a>
+                        </p>
+
+                    </div>
+                    <div class="">
+                        <img width="80px" style="margin-right: 10px" class="lazy"src="{{asset("./web/static/images/WechatIMG5.jpeg")}}">
+                        <img width="80px" class="lazy"src="{{asset("./web/static/images/WechatIMG6.jpeg")}}">
                     </div>
                 </div>
             </div>
+        </div>
             <!-- Contact end -->
 
         </div>
--- resources/views/web/we/page.blade.php
+++ resources/views/web/we/page.blade.php
@@ -84,17 +84,17 @@
                     <div class="panel-bd">
                         <div class="">
                             <h3>快象文化传播（上海）有限公司 </h3>
-                            <p><span>地址：</span><a href="">{{$sstt->homedata()->address}}</a></p>
+                            <p><span>地址：</span><a href="">{{$sstt->homedata()->address}}</a></p><br/>
                             <p><span>电话：</span><a href="tel:{{$sstt->homedata()->phone}}">{{$sstt->homedata()->phone}} </a>
-                            </p>
+                            </p><br/>
                             <p><span>网址：</span><a href="{{$sstt->homedata()->url}}">{{$sstt->homedata()->url}}</a>
-                            </p>
+                            </p><br/>
                             <p><span>备案号：</span><a >沪ICP备17052966号-1</a>
                             </p>
 
                         </div>
                         <div class="">
-                            <img width="80px" class="lazy"src="{{asset("./web/static/images/WechatIMG5.jpeg")}}">
+                            <img width="80px" style="margin-right: 10px" class="lazy"src="{{asset("./web/static/images/WechatIMG5.jpeg")}}">
                             <img width="80px" class="lazy"src="{{asset("./web/static/images/WechatIMG6.jpeg")}}">
                         </div>
                     </div>
